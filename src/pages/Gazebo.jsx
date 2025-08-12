d45ffb36da97ed0dcc7153a77c206ceee4dc659d
PPZ020 <shivammadhwal.ppz020@gmail.com>
2025-08-12 10:23:47 +0530

    hi

--- src/pages/Gazebo.jsx
+++ src/pages/Gazebo.jsx
@@ -143,7 +143,7 @@ const Gazebo = () => {
         </div>
 
         {/* Image Grid */}
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 gap-4">
           {images.map((img, index) => {
             const [ref, visible] = useInView();
             return (
